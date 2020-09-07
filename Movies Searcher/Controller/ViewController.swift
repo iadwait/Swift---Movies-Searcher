556d5c613ca744fd1a8267333ab480597336c93d
iAdwait <adwaitbarkale@gmail.com>
2020-09-07 15:34:44 +0530

    Extension Updated

--- Movies Searcher/Controller/ViewController.swift
+++ Movies Searcher/Controller/ViewController.swift
@@ -93,21 +93,3 @@ class ViewController: UIViewController,UITextFieldDelegate,UITableViewDelegate,U
 }
 
 
-extension UIImageView
-{
-    func fetchImage(fromURL: String)
-    {
-        guard let imgURL = URL(string: fromURL) else { return }
-        let session = URLSession(configuration: .default)
-        let task = session.dataTask(with: imgURL) { (data, response, err) in
-            if let imgData = data,err == nil {
-                DispatchQueue.main.async {
-                    let image = UIImage(data: imgData)
-                    self.image = image
-                }
-                
-            }
-        }
-        task.resume()
-    }
-}
